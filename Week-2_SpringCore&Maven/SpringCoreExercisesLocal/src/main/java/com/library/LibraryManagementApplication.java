264c1fa181a6d8d8ca403922f555f39a27509fa0
prakyats <shettyprakyat15@gmail.com>
2026-06-20 15:27:06 +0530

    Completed Spring Core and Maven Exercise 1

---
+++ Week-2_SpringCore&Maven/SpringCoreExercisesLocal/src/main/java/com/library/LibraryManagementApplication.java
@@ -0,0 +1,24 @@
+package com.library;
+
+import com.library.repository.BookRepository;
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryManagementApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService bookService =
+                context.getBean("bookService", BookService.class);
+
+        BookRepository bookRepository =
+                context.getBean("bookRepository", BookRepository.class);
+
+        bookService.addBook();
+        bookRepository.displayBook();
+    }
+}
---
+++ Week-2_SpringCore&Maven/SpringCoreExercisesLocal/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,8 @@
+package com.library.service;
+
+public class BookService {
+
+    public void addBook() {
+        System.out.println("Book service is working.");
+    }
+}
