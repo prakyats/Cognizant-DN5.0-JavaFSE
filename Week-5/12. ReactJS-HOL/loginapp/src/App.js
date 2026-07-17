ea208a277fd7fa46d0317c2ccebebbe6f3277509
prakyats <shettyprakyat15@gmail.com>
2026-07-17 08:38:08 +0530

    Completed 12. ReactJS-HOL

---
+++ Week-5/12. ReactJS-HOL/loginapp/src/App.js
@@ -0,0 +1,12 @@
+import LoginControl from "./LoginControl";
+
+function App() {
+
+    return (
+        <div style={{ marginTop: "100px", marginLeft: "220px" }}>
+            <LoginControl />
+        </div>
+    );
+}
+
+export default App;
---
+++ Week-5/12. ReactJS-HOL/loginapp/src/UserGreeting.js
@@ -0,0 +1,5 @@
+function UserGreeting() {
+    return <h2>Welcome back</h2>;
+}
+
+export default UserGreeting;
